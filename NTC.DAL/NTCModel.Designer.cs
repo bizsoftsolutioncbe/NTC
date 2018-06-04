b9d02377a7235d98ebec348acc39793997f70d55
bala <bala.bizsoft@gmail.com>
2018-06-04 12:20:23 +0530

    finished PropertyDetail

--- NTC.DAL/NTCModel.Designer.cs
+++ NTC.DAL/NTCModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'c:\users\bala\source\repos\NTC\NTC.DAL\NTCModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Bala\source\repos\NTC\NTC.DAL\NTCModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
